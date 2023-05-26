185bce5438d3cd96c9890f702a6c6b96e7bcb137
Aan Nea <90422412+aannea@users.noreply.github.com>
2023-05-26 10:55:07 +0700

    Create 006_InputAndOutput.cpp

---
+++ 006_InputAndOutput.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int angka; //angka dimasukkan dalam variable integer
+    char karakter; //karakter dimasukkan dalam variable character
+    string kata; // kata dimasukkan dalam variable string
+
+    cout << "Inputkan angka : ";
+    cin >> angka; // penginputan nilai dari angka
+    cout << "Inputkan karakter : ";
+    cin >> karakter; // penginputan karakter
+    cout << "Inputkan kata : ";
+    cin >> kata; // penginputan kata
+
+    cout << endl << "Hasil inputan adalah : " << endl; //akan menampilkan dari inputan yang tadi di isi
+    cout << angka << endl; // menampilkan hasil inputan angka
+    cout << karakter << endl; // menampilkan hasil inputan karakter
+    cout << kata << endl; // menampilkan hasil inputan kata
+
+    return 0;
+}
