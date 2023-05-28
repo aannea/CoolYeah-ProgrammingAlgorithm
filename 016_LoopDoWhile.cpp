922be817831024baa8bed799c923741c46b6d847
Aan Nea <90422412+aannea@users.noreply.github.com>
2023-05-28 22:22:46 +0700

    Create 016_LoopDoWhile.cpp

---
+++ 016_LoopDoWhile.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int c=0;//meamasukkan nilai c kedalam integer dan menetapkan nilainya
+
+    do{ //periulangan menggunakan do, dan menampilkan dulu baru ditetapkan
+            //berapa nilai diulangnya bepara kali
+        cout<<"saya sangat menyukai C++"<<endl;//tampilan
+        c++;//pertambahaan nilai c
+    } while (c<5);//mengidentifiikasi nilai perulangan berapa kali
+    return 0;
+}
