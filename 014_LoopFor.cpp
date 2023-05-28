4ffc701309dca82041e399e28fc551d4a2735b09
Aan Nea <90422412+aannea@users.noreply.github.com>
2023-05-28 12:59:44 +0700

    Create 014_LoopFor.cpp

---
+++ 014_LoopFor.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int c; //variable c dimasukkan dalam type data integer
+    for (c=0; c<5; c++){ //prulangan menggunakan for
+        cout << "Saya sangat menyukai C++" << endl;
+    //tampilan yang muncul berulang
+    }
+    return 0;
+}
