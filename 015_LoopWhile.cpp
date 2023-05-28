26b083f2fbb737ecbec393cf9d7ace0d8baf0420
Aan Nea <90422412+aannea@users.noreply.github.com>
2023-05-28 22:21:24 +0700

    Create 015_LoopWhile.cpp

---
+++ 015_LoopWhile.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int c; //memasukkan variable c kedalam integer
+    c=0;//menetapkan nilai awal dari variable c
+    while (c<5){//perulangan menggunakan while
+        cout << "Saya sangat menyukai C++"<<endl;//tampilan
+        c++;//penambahan nilai c, agar perulangan mencapai target
+    }
+    return 0;
+}
