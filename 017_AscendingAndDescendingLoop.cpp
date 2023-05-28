5c85f6bb84688d83aeffe2a2435343f0ff51bfc5
Aan Nea <90422412+aannea@users.noreply.github.com>
2023-05-28 22:25:02 +0700

    Create 017_AscendingAndDescendingLoop.cpp

---
+++ 017_AscendingAndDescendingLoop.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "PERULANGAN MENAIK" << endl;//tampilan
+    for (int c=0; c<5; c++){//perulangan menggunakan for, dan langsung
+        //menetapkan nilai dari c dan masuk pada type data integer
+        cout << c+1 << endl;//tampilan
+    }
+    cout <<"\n";//jarak
+    cout << "PERULAGAN MENURUN" << endl;//tampilan
+    for (int j=5; j>0; j--){//perualangan menggunakan for, dan langung
+        //menetapkan nilai dari j dan masuk pada type data integer
+    cout << j << endl;//tampilan
+    }
+    return 0;
+}
